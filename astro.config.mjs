806dec2dc43f1aca6f0f2c8e2f98f7ec85768cb4
Wes Furgason <wfurgason@gmail.com>
2026-03-25 01:17:36 -0600

    simplify astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://magnaarts.org',
-  integrations: [sitemap()],
 });

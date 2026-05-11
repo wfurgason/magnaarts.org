9760a32eca03fc3df59e918d549197dce6df74b4
Wes Furgason <wfurgason@Mac.lan>
2026-05-11 01:11:27 -0600

    optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,7 @@ export default defineConfig({
   output: 'server',
   adapter: vercel(),
   site: 'https://magnaarts.org',
+  build: {
+    inlineStylesheets: 'auto', // inline CSS files under 4 KB to eliminate render-blocking stylesheet requests
+  },
 });
